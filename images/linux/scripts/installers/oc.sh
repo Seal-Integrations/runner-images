d4a83d43c4cb1f240d7f2c73079428b0682fc3ba
Shaked Shauli <shaked.s@sealsecurity.io>
2024-09-04 13:46:14 +0300

    more

--- images/linux/scripts/installers/oc.sh
+++ images/linux/scripts/installers/oc.sh
@@ -7,7 +7,7 @@
 source $HELPER_SCRIPTS/install.sh
 
 # Install the oc CLI
-DOWNLOAD_URL="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz"
+DOWNLOAD_URL="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.1.0/openshift-client-linux-4.1.0.tar.gz"
 PACKAGE_TAR_NAME="oc.tar.gz"
 download_with_retries $DOWNLOAD_URL "/tmp" $PACKAGE_TAR_NAME
 tar xzf "/tmp/$PACKAGE_TAR_NAME" -C "/usr/local/bin" oc
